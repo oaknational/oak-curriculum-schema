4e5b5f230203c4989fe04460f21332c1667237de
Simon Katan <simon.katan@thenational.academy>
2024-04-18 16:11:38 +0100

    feat: added keystage schema data

--- src/schema/base.schema.ts
+++ src/schema/base.schema.ts
@@ -2,3 +2,18 @@ import { z } from "zod";
 
 export const _stateSchema = z.enum(["published", "new", "migration"]);
 export const _cohortSchema = z.enum(["2020-2023", "2023-2024", "0"]);
+export const keystages = z.enum(["KS1", "KS2", "KS3", "KS4", "EYFS"]);
+export const keystageSlugs = z.enum([
+  "ks1",
+  "ks2",
+  "ks3",
+  "ks4",
+  "early-years-foundation-stage",
+]);
+export const keystageDescriptions = z.enum([
+  "Key Stage 1",
+  "Key Stage 2",
+  "Key Stage 3",
+  "Key Stage 4",
+  "Early Years Foundation Stage",
+]);
