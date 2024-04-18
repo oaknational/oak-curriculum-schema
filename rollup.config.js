0c4db4c50969c51fd7bbe5ea456eec463f8148b1
Simon Katan <simon.katan@thenational.academy>
2024-04-18 10:42:09 +0100

    feat: configuring rollup

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
+import { typescriptPaths } from "rollup-plugin-typescript-paths";
+
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+        interop: "compat", // https://rollupjs.org/configuration-options/#output-interop - needed for nextjs
+      },
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+        interop: "compat", // https://rollupjs.org/configuration-options/#output-interop - needed for nextjs
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      nodeResolve({ preferBuiltins: true }),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
+      commonjs(),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/types.d.ts", format: "es" }],
+    plugins: [typescriptPaths({ preserveExtensions: true }), dts.default()],
+  },
+];
