d8df181cb9ee83f431873c2a551105218298aa79
Simon Katan <simon.katan@thenational.academy>
2024-04-22 08:30:08 +0100

    feat: addinging extra schemas

---
+++ src/schema/syntheticUnitvariantLessons.schema.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import { lessonDataSchema } from "./lessonData.schema";
+import { unitDataSchema } from "./unitData.schema";
+import { unitvariantSchema } from "./unitvariant.schema";
+import { programmeFieldsSchema } from "./programmeFields.schema";
+
+export const syntheticUnitvariantLessonsSchema = z.object({
+  lesson_slug: z.string(),
+  unit_slug: z.string(),
+  programme_slug: z.string(),
+  is_legacy: z.boolean(),
+  lesson_data: lessonDataSchema,
+  unit_data: unitDataSchema,
+  null_unitvariant: unitvariantSchema,
+  programme_fields: programmeFieldsSchema,
+  supplementary_data: z.object({
+    unit_order: z.number(),
+    order_in_unit: z.number(),
+  }),
+});
+
+export type SyntheticUnitvariantLessons = z.infer<
+  typeof syntheticUnitvariantLessonsSchema
+>;
