b6794f35e5f0f96f9fc66fac8f053b8edb299c8a
Simon Katan <simon.katan@thenational.academy>
2024-04-29 10:37:56 +0100

    feat: update synthetic programmes to match v 7_1_0

--- src/schema/syntheticProgrammesByYear.schema.ts
+++ src/schema/syntheticProgrammesByYear.schema.ts
@@ -1,19 +1,13 @@
 import { z } from "zod";
 import { programmeFieldsSchema } from "./programmeFields.schema";
-import { examboardSlugs, tierSlugs, yearSlugs } from "./base.schema";
+import { yearSlugs } from "./base.schema";
 
 export const syntheticProgrammesByYearSchema = z.object({
   year_slug: yearSlugs,
   programme_slug: z.string(),
   base_slug: z.string(),
-  synthetic_programme_slug: z.object({
-    full_slug: z.string(),
-    is_legacy: z.boolean(),
-    tier_slug: tierSlugs.nullable(),
-    examboard_slug: examboardSlugs.nullable(),
-  }),
-  combined_programme_fields: programmeFieldsSchema.partial(),
-  base_programme_fields: programmeFieldsSchema.partial(),
+  is_legacy: z.boolean(),
+  programme_fields: programmeFieldsSchema,
 });
 
 export type SyntheticProgrammesByYear = z.infer<
