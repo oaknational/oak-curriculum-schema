7807ccab8451a3ee02df6acdfe938464a3a4e861
Kyle Huggins <91190841+k-huggs@users.noreply.github.com>
2025-01-20 18:05:58 +0000

    chore: remove excess data

--- src/fixtures/mediaClips.fixture.ts
+++ src/fixtures/mediaClips.fixture.ts
@@ -18,45 +18,17 @@ export const mediaClipsFixture = ({
           url: "http://example.com/video1.mp3",
           type: "upload",
           bytes: 81127,
-          width: 0,
           format: "mp3",
-          height: 0,
-          version: 1736860290,
           duration: 5.041633,
-          metadata: {
-            asset_type: "audio_speech",
-            source_type: "partner_created",
-            asset_source: "partner_created",
-            licence_type: "no_licence",
-            content_cycle: "cycle_2",
-            permissionGranted: "notRequired",
-          },
-          secure_url: "https://example.com/video1.mp3",
-          access_mode: "public",
-          asset_folder:
-            "Cycle 2 assets/MFL - French - Secondary/Audio/Y7/FR7U15/FR7U15L1",
+
           display_name: "9_task_C1_3",
           resource_type: "video",
         },
         video_object: {
           id: "hug9i01Tnpf1y83irOm00HRbvAJpttJPU78KNYzPav3mg",
-          status: "ready",
-          tracks: [
-            {
-              id: "32v67aMKIr5Z014b3n3AVwNHjAPnWSuhJCm008E026lYOQ",
-              type: "audio",
-              duration: 5.041633,
-            },
-            {
-              id: "staging-mock-asset",
-              asset_id: "hug9i01Tnpf1y83irOm00HRbvAJpttJPU78KNYzPav3mg",
-            },
-          ],
+
           duration: 5.055667,
 
-          created_at: 1736864397,
-          mp4_support: "standard",
-          passthrough: "VIDE-ZHHRL-29844",
           mux_asset_id: "hug9i01Tnpf1y83irOm00HRbvAJpttJPU78KNYzPav3mg",
           playback_ids: [
             {
@@ -68,15 +40,7 @@ export const mediaClipsFixture = ({
               policy: "public",
             },
           ],
-          encoding_tier: "smart",
-          video_quality: "plus",
           mux_playback_id: "BW00NkK9R01jB8PPO7R00YCFl2XBDn13GTkhd0001PNtheF00",
-          signed_stream_id: "mVkKUtOfoD1100012GNC1pCO6RvUgyGwqGoq01pYsy7WeA",
-          static_renditions: {
-            status: "preparing",
-          },
-          max_resolution_tier: "1080p",
-          max_stored_resolution: "Audio only",
         },
       },
       {
@@ -90,45 +54,17 @@ export const mediaClipsFixture = ({
           url: "http://example.com/video2.mp3",
           type: "upload",
           bytes: 122087,
-          width: 0,
           format: "mp3",
-          height: 0,
-          version: 1736860289,
           duration: 7.601633,
-          metadata: {
-            asset_type: "audio_speech",
-            source_type: "partner_created",
-            asset_source: "partner_created",
-            licence_type: "no_licence",
-            content_cycle: "cycle_2",
-            permissionGranted: "notRequired",
-          },
-          secure_url: "https://example.com/video2.mp3",
-          access_mode: "public",
-          asset_folder:
-            "Cycle 2 assets/MFL - French - Secondary/Audio/Y7/FR7U15/FR7U15L1",
+
           display_name: "8_task_C1_2",
           resource_type: "video",
         },
         video_object: {
           id: "gyUmSG2VVqcuw00NzT9f02kZvlLmXsrnuT5P7KhrYhWJg",
-          status: "ready",
-          tracks: [
-            {
-              id: "01hRHlW4zKJxPMUCtlSHQLaQjecx9o02gNsy597cLs01500",
-              type: "audio",
-              duration: 7.601633,
-            },
-            {
-              id: "staging-mock-asset",
-              asset_id: "gyUmSG2VVqcuw00NzT9f02kZvlLmXsrnuT5P7KhrYhWJg",
-            },
-          ],
+
           duration: 7.603667,
 
-          created_at: 1736864397,
-          mp4_support: "standard",
-          passthrough: "VIDE-TVKWF-29845",
           mux_asset_id: "gyUmSG2VVqcuw00NzT9f02kZvlLmXsrnuT5P7KhrYhWJg",
           playback_ids: [
             {
@@ -140,15 +76,7 @@ export const mediaClipsFixture = ({
               policy: "signed",
             },
           ],
-          encoding_tier: "smart",
-          video_quality: "plus",
           mux_playback_id: "9a02PY7PivjOBUHyH4N2mAwJH00aJoZeybWyy9hiwXVQY",
-          signed_stream_id: "02mDhMdHMs4MOCAMutPLWzylp00NQgDYfiydlLQPDWI3M",
-          static_renditions: {
-            status: "preparing",
-          },
-          max_resolution_tier: "1080p",
-          max_stored_resolution: "Audio only",
         },
       },
     ],
@@ -164,52 +92,17 @@ export const mediaClipsFixture = ({
           url: "http://oaknationalacademy-res.cloudinary.com/video/upload/v1736420414/u5bptym7h8ts8nyyrpwg.mp4",
           type: "upload",
           bytes: 276767,
-          width: 1920,
           format: "mp4",
-          height: 1080,
-          version: 1736420414,
           duration: 5.613111,
-          metadata: {
-            asset_type: "video_other",
-            source_type: "partner_created",
-            asset_source: "partner_created",
-            licence_type: "no_licence",
-            content_cycle: "cycle_2",
-            permissionGranted: "notRequired",
-          },
 
-          secure_url:
-            "https://oaknationalacademy-res.cloudinary.com/video/upload/v1736420414/u5bptym7h8ts8nyyrpwg.mp4",
-          access_mode: "public",
-
-          asset_folder: "Test - Internal use only",
           display_name: "australia fact",
           resource_type: "video",
         },
         video_object: {
           id: "4yb74fNKob6M6AT02AFTmsTrJEgXUl00WQWhpt01KttWcI",
-          status: "ready",
-          tracks: [
-            {
-              id: "A6wEzIy8eVnOiRczG6027ibMXQQ02vBLWrfsXYfza4kEE",
-              type: "audio",
-              duration: 5.58,
-            },
-            {
-              id: "8WDoX54TtLy7r02ZACcYYJAWunpvVsayudUrZ8JoGOBA",
-              type: "video",
-              duration: 5.566667,
-            },
-            {
-              id: "staging-mock-asset",
-              asset_id: "4yb74fNKob6M6AT02AFTmsTrJEgXUl00WQWhpt01KttWcI",
-            },
-          ],
+
           duration: 5.613111,
 
-          created_at: 1736955070,
-          mp4_support: "standard",
-          passthrough: "VIDE-NSHCJ-29877",
           mux_asset_id: "4yb74fNKob6M6AT02AFTmsTrJEgXUl00WQWhpt01KttWcI",
           playback_ids: [
             {
@@ -221,15 +114,8 @@ export const mediaClipsFixture = ({
               policy: "signed",
             },
           ],
-          encoding_tier: "smart",
-          video_quality: "plus",
+
           mux_playback_id: "RUIUNGcGf01kgZU1xC5peDCvGpC2d2YeRcwSMfJ4cvMk",
-          signed_stream_id: "NVs5TJ5Lt6POkNwDBNtHZth2a2VtBvsyorgJPQfO6W4",
-          static_renditions: {
-            status: "preparing",
-          },
-          max_resolution_tier: "1080p",
-          max_stored_resolution: "HD",
         },
       },
     ],
--- src/schema/mediaClips.schema.ts
+++ src/schema/mediaClips.schema.ts
@@ -1,79 +1,53 @@
 import { z } from "zod";
 
-const metadataSchema = z
-  .object({
-    asset_type: z.string(),
-    source_type: z.string(),
-    asset_source: z.string(),
-    licence_type: z.string(),
-    content_cycle: z.string(),
-    permissionGranted: z.string(),
-  })
-  .partial();
+// TODO: Schema is not complete so until data & structure is consistent is known partial() & nullish() being implemented
 
-export const clipMediaObjectSchema = z.object({
-  id: z.string(),
-  url: z.string(),
-  type: z.string(),
-  bytes: z.number(),
-  width: z.number(),
-  format: z.string(),
-  height: z.number(),
-  version: z.number(),
-  duration: z.number(),
-  metadata: metadataSchema,
-  secure_url: z.string(),
-  access_mode: z.string(),
-  asset_folder: z.string(),
-  display_name: z.string(),
-  resource_type: z.string(),
-});
-
-const trackSchema = z
+export const clipMediaObjectSchema = z
   .object({
     id: z.string(),
+    url: z.string(),
     type: z.string(),
-    asset_id: z.string(),
-    duration: z.number(),
+    bytes: z.number(),
+    format: z.string(),
+    duration: z.number().nullable(),
+    display_name: z.string(),
+    resource_type: z.string(),
   })
-  .partial();
+  .partial()
+  .nullish();
 
-export const clipVideoObjectSchema = z.object({
-  id: z.string(),
-  status: z.string().optional(),
-  tracks: z.array(trackSchema).optional(),
-  duration: z.number(),
-  created_at: z.number(),
-  mp4_support: z.string(),
-  passthrough: z.string(),
-  mux_asset_id: z.string(),
-  playback_ids: z.array(
-    z.object({
-      id: z.string(),
-      policy: z.string(),
-    }),
-  ),
-  encoding_tier: z.string(),
-  video_quality: z.string(),
-  mux_playback_id: z.string(),
-  signed_stream_id: z.string(),
-  static_renditions: z.object({
-    status: z.string(),
-  }),
-  max_resolution_tier: z.string(),
-  max_stored_resolution: z.string(),
-});
+export const clipVideoObjectSchema = z
+  .object({
+    id: z.string(),
+    duration: z.number().nullable(),
+    mux_asset_id: z.string(),
+    playback_ids: z.array(
+      z
+        .object({
+          id: z.string(),
+          policy: z.string(),
+        })
+        .partial()
+        .nullish(),
+    ),
+    mux_playback_id: z.string(),
+  })
+  .partial()
+  .nullish();
 
-export const mediaClipCycleSchema = z.object({
-  // Test data had mixture of numbers and strings
-  order: z.number().or(z.string()),
-  media_id: z.number().or(z.string()),
-  video_id: z.number(),
-  media_type: z.string(),
-  custom_title: z.string(),
-  media_object: clipMediaObjectSchema,
-  video_object: clipVideoObjectSchema,
-});
+export const mediaClipCycleSchema = z
+  .object({
+    // Test data had mixture of numbers and strings
+    order: z.number().or(z.string()),
+    media_id: z.number().or(z.string()),
+    video_id: z.number().nullable(),
+    media_type: z.string().nullish(),
+    custom_title: z.string().nullish(),
+    media_object: clipMediaObjectSchema,
+    video_object: clipVideoObjectSchema,
+  })
+  .partial()
+  .nullish();
 
 export const mediaClipsRecordSchema = z.record(
   z.string(),
