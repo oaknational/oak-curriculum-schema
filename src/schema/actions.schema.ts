9e27029f56ad2d166f7d89a03fb3db18cf385016
Weronika Szalas <freelance.wszalas@thenational.academy>
2025-02-10 15:01:47 +0100

    feat: add override_pathway to actions schema

--- src/schema/actions.schema.ts
+++ src/schema/actions.schema.ts
@@ -35,5 +35,8 @@ export const actionsSchema = z
     disable_pupil_share: z.boolean(),
     display_PE_title: z.boolean(),
     display_vocab_button: z.boolean(),
+    override_pathway: z.string(), // we have to override pathway in this way because we don't have the functionality to override different selection of programme fields across different queries
   })
   .partial();
+
+export type Actions = z.infer<typeof actionsSchema>;
