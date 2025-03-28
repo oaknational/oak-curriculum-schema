58d2ac06a155ca5b4a3ebab17f5b504e0dfb3ce8
ben <ben.protheroe@thenational.academy>
2025-03-28 13:41:53 +0000

    feat: add features schema

--- src/schema/syntheticProgrammesByYear.schema.ts
+++ src/schema/syntheticProgrammesByYear.schema.ts
@@ -10,7 +10,10 @@ export const syntheticProgrammesByYearSchema = z.object({
   is_legacy: z.boolean(),
   programme_fields: programmeFieldsSchema,
   actions: actionsSchema.nullable().optional(), // the optional should be removed once old mvs are retired
-  features: z.object({}).optional().nullable(), // the optional should be removed once old mvs are retired
+  features: z
+    .object({ nonCurriculum: z.boolean().optional() })
+    .optional()
+    .nullable(), // the optional should be removed once old mvs are retired
 });
 
 export type SyntheticProgrammesByYear = z.infer<
