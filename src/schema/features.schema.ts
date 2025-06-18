b21b8bbdf0082de0e231e07fe05a393983c5077b
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2025-06-18 13:58:46 +0100

    feat: add two fields to features schema
    - geo restricted
    - login required

--- src/schema/features.schema.ts
+++ src/schema/features.schema.ts
@@ -4,6 +4,8 @@ export const featuresSchema = z
   .object({
     pe_practical: z.boolean(),
     age_restriction: z.string().nullable(),
+    agf__geo_restricted: z.boolean().nullish(),
+    agf__login_required: z.boolean().nullish(),
   })
   .partial();
 
