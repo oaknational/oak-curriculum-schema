7dc82608bfb04bac78e2488669e12d8b2a77c7b1
thomas-o-chan <thomas.chan@thenational.academy>
2025-11-25 16:20:56 +0000

    feat: add unitvariant new and published schemas

--- src/schema/public/unitvariant/unitvariant.schema.ts
+++ src/schema/public/unitvariant/unitvariant.schema.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
 import zodToCamelCase from "zod-to-camel-case";
 
-import { _stateSchema, _cohortSchema } from "@/schema/public/base/base.schema";
+import {
+  _stateSchema,
+  _cohortSchema,
+  newState,
+  publishedState,
+} from "@/schema/public/base/base.schema";
 import { unitDataSchema } from "@/schema/public/unitData/unitData.schema";
 import { programmeFieldsSchema } from "@/schema/public/programmeFields/programmeFields.schema";
 
@@ -16,5 +21,35 @@ export const unitvariantSchema = z.object({
 });
 export type Unitvariant = z.infer<typeof unitvariantSchema>;
 
+export const unitvariantNewSchema = unitvariantSchema
+  .pick({
+    unit_id: true,
+    unitvariant_id: true,
+    unit_overrides: true,
+    programme_fields: true,
+    _cohort: true,
+    _deleted: true,
+  })
+  .extend({
+    _state: newState,
+  });
+
+export type UnitvariantNew = z.infer<typeof unitvariantNewSchema>;
+
+export const unitvariantPublishedSchema = unitvariantSchema.extend({
+  _state: publishedState,
+});
+export type UnitvariantPublished = z.infer<typeof unitvariantPublishedSchema>;
+
 export const unitvariantSchemaCamel = zodToCamelCase(unitvariantSchema);
 export type UnitvariantCamel = z.infer<typeof unitvariantSchemaCamel>;
+
+export const unitvariantNewSchemaCamel = zodToCamelCase(unitvariantNewSchema);
+export type UnitvariantNewCamel = z.infer<typeof unitvariantNewSchemaCamel>;
+
+export const unitvariantPublishedSchemaCamel = zodToCamelCase(
+  unitvariantPublishedSchema,
+);
+export type UnitvariantPublishedCamel = z.infer<
+  typeof unitvariantPublishedSchemaCamel
+>;
